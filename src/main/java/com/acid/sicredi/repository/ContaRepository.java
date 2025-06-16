f6238a92f1222022a50f269e1af3a127a7e32570
erick <erick.cabral122@gmail.com>
2025-06-16 14:07:18 -0300

    feat: repository add

--- src/main/java/com/acid/sicredi/repository/ContaRepository.java
+++ src/main/java/com/acid/sicredi/repository/ContaRepository.java
@@ -1,4 +1,13 @@
 package com.acid.sicredi.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.acid.sicredi.model.Conta;
+
 public class ContaRepository {
+
+    @Repository
+    public interface ContaRepository extends JpaRepository<Conta, Long>{
+
+    }
 }
---
+++ src/main/java/com/acid/sicredi/service/TransferenciaService.java
@@ -0,0 +1,4 @@
+package com.acid.sicredi.service;
+
+public class TransferenciaService {
+}
